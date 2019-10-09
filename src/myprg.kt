8cde3b61c4ab947044e860d39e4b73dcbaa90c8f
Sergey Kovalev <kaermor@gmail.com>
2019-10-09 16:11:58 +0300

    Step 2-2-3. Better notation for solution!!

--- src/myprg.kt
+++ src/myprg.kt
@@ -1,13 +1,11 @@
 fun getYearEra(year: Int): String{
-    var tt = "Unix"
-    when{
-        year < 1970 -> tt = "before"
-        year == 1970 -> tt = "equals"
-        year in 1971..2000 -> tt = "after (XX century)"
-        year in 2001..2100 -> tt = "after (XXI century)"
-        year > 2100 -> tt = "distant future"
+    return when{
+        year < 1970 -> "before"
+        year == 1970 -> "equals"
+        year in 1971..2000 -> "after (XX century)"
+        year in 2001..2100 -> "after (XXI century)"
+        else -> "distant future"
     }
-    return tt
 //    return "Forever Unix"
 
 }
