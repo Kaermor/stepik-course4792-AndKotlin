8fdda156307c190ce191d20969810cbc4d2cede2
Sergey Kovalev <kaermor@gmail.com>
2019-10-09 15:49:36 +0300

    Step 2-2-3. Solved *"complex" task.

--- src/myprg.kt
+++ src/myprg.kt
@@ -1,17 +1,18 @@
-fun main(args: Array<String>){
-    val sstr:String? = readLine()
-    var digit: Int
-    when(sstr){
-        "one" -> digit=1
-        "two" -> digit=2
-        "three" -> digit=3
-        "four" -> digit=4
-        "five" -> digit=5
-        "zero" -> digit=0
-        else -> {
-            println("Wrong string!")
-            return
-        }
+fun getYearEra(year: Int): String{
+    var tt = "Unix"
+    when{
+        year < 1970 -> tt = "before"
+        year == 1970 -> tt = "equals"
+        year in 1971..2000 -> tt = "after (XX century)"
+        year in 2001..2100 -> tt = "after (XXI century)"
+        year > 2100 -> tt = "distant future"
     }
-    println(digit)
+    return tt
+//    return "Forever Unix"
+
+}
+
+fun main(args: Array<String>){
+    val year:Int = readLine()!!.toInt()
+    println(getYearEra(year))
 }
