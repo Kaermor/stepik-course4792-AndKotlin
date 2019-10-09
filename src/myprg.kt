e5f9aaa22e260c0be309b066070f6e47345cc437
Sergey Kovalev <kaermor@gmail.com>
2019-10-09 16:27:12 +0300

    Step 2-2-5. Example from video lecture. But i'm used str!!

--- src/myprg.kt
+++ src/myprg.kt
@@ -1,16 +1,8 @@
-fun getYearEra(year: Int): String{
-    return when{
-        year < 1970 -> "before"
-        year == 1970 -> "equals"
-        year in 1971..2000 -> "after (XX century)"
-        year in 2001..2100 -> "after (XXI century)"
-        else -> "distant future"
-    }
-//    return "Forever Unix"
-
-}
-
 fun main(args: Array<String>){
-    val year:Int = readLine()!!.toInt()
-    println(getYearEra(year))
+    val str:String? = readLine()
+    var zeros_count:Int = 0
+    for (c in str!!){
+        if (c == '0') zeros_count++
+    }
+    println("${zeros_count} zeros")
 }
