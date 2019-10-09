16df1753297693c2caad45c2ddcaa94636f39c5e
Kaermor <kaermor@gmail.com>
2019-10-09 18:20:33 +0300

    Step 2-2-6. Task solution in trivial way (https://kotlinlang.org/docs/reference/basic-types.html#characters)

--- src/myprg.kt
+++ src/myprg.kt
@@ -1,12 +1,10 @@
 fun calculateEvenDigits(input:String): Int{
     var sum = 0
     println(input)
-    for (c in input){
-        println("${c} ${c.toInt()} ${c.toInt()%2}")
+    for (c:Char in input){
+        //println("${c} ${c.toInt()-'0'.toInt()} ${c.toInt()%2}")
 
-        if (c.toInt()%2 == 0){
-            sum+=c.toInt()
-        }
+        if ((c.toInt()-'0'.toInt())%2 == 0) sum+=c.toInt()-'0'.toInt()
     }
     return sum
 }
