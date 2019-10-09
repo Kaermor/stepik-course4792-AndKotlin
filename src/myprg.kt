6c46915f219b5b6653db5a7641c1b401b94fa6f6
Sergey Kovalev <kaermor@gmail.com>
2019-10-09 15:02:50 +0300

    Step 2-2-2. Example from video lecture.

--- src/myprg.kt
+++ src/myprg.kt
@@ -1,10 +1,17 @@
-fun calcBugMoneyValue(dayNumber: Int, bugRank: Int, cashAmount: Int): Int {
-    return (dayNumber*bugRank + 42)*cashAmount
-}
-
-fun main(args: Array<String>) {
-    val (dayNumber:Int, bugRank:Int, cashAmount:Int) = readLine()!!.split(' ').map(String::toInt)
-
-    println(calcBugMoneyValue(dayNumber, bugRank, cashAmount))
-//    println("${dayNumber*2} ${bugRank*2} ${cashAmount*2}")
+fun main(args: Array<String>){
+    val sstr:String? = readLine()
+    var digit: Int
+    when(sstr){
+        "one" -> digit=1
+        "two" -> digit=2
+        "three" -> digit=3
+        "four" -> digit=4
+        "five" -> digit=5
+        "zero" -> digit=0
+        else -> {
+            println("Wrong string!")
+            return
+        }
+    }
+    println(digit)
 }
