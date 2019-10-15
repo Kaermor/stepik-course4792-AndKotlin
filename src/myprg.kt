89863898acca9f3951b8c7e1b24e63016190add9
Sergey Kovalev <kaermor@gmail.com>
2019-10-15 12:20:38 +0300

    Step 2-4-5. Solved.

--- src/myprg.kt
+++ src/myprg.kt
@@ -1,21 +1,11 @@
-open class Bug(val rank: Int, val name:String) {
-    open fun getSugarLimit(): Int{
-        return rank
-    }
-
-    fun getId(): String{
-        return "${rank}.${name}"
-    }
-}
-
-class  BugCivilian(rank: Int, name: String):Bug(rank, name){
-    override fun getSugarLimit(): Int {
-        return super.getSugarLimit()/2
-    }
+fun getCubeList(n:Int): List<Int>{
+    // enter your function code here
+    return List(n, {i -> i*i*i})
 }
 
 
 fun main(args: Array<String>){
-//    val instance: BugCivilian = BugCivilian()
-//    println("Damage = ${instance.calcDamage()}")
+    val n: Int = readLine()!!.toInt()
+    println(getCubeList(n))
+
 }
